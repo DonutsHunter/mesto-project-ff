3671fba39aaee50e6888cb75bc06b6c0a92335c4
Nikolay Vashentsev <DonutsHunter@yandex.ru>
2024-12-28 15:49:27 +0300

    Добавление базовой функциональности

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// @todo: Функция добавления карточки
+function addCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCallback(cardElement);
+  });
+
+  // Добавляем карточку в DOM
+  placesList.appendChild(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  addCard(cardData, deleteCard);
+});
